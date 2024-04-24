1affd146733c97eed44a2b1a9c1099bafeb654fb
Pedro <pedro.soares@unimedanapolis.com.br>
2024-04-24 12:28:46 -0300

    mudança dia 24/04/24

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./index.css";
-
+  import pokelogo from "./assets/pokelogo.png";
 export default function App() {
   const [data, setData] = useState(null);
   const [pokemonName, setPokemonName] = useState("");
@@ -19,7 +19,7 @@ export default function App() {
     };
 
     fetchData();
-  }, [pokemonName]);
+  }, [pokemonName, data]);
 
   const handleInputChange = (event) => {
     const name = event.target.value;
@@ -30,29 +30,42 @@ export default function App() {
     return <>Carregando...</>;
   }
 
-  const actualImage = data?.sprites?.front_default || ''
-
+  const actualImage = data?.sprites?.front_default || pokelogo;
+  const verify = data.types && data.types.length > 0 && data.types[0].type && data.types[0].type.name ;
   return (
     <div className="container">
       <div className="texto">Digite o nome do Pokémon desejado: </div>
-      <div className="cards">
+      <div class="card" style={{
+        backgroundColor:(verify === 'fire' ) ? 'red' : 'white'
+        }}>
         <div className="header-card">
           <div className="input-area">
             <input type="text" onChange={handleInputChange} />
+            <div className="tipo">Tipo: {verify ? data.types[0].type.name : ''}</div>
           </div>
+        
+         <div className="card-image">
+             {
+              pokemonName === '' ? (
+                <img src={pokelogo}/>
+                
+              ) : (
+                <img src={actualImage}/>
+              )
+             }
+             
 
-          <div className="card-info">
-            <p>Nome: {data.name}</p>
-            <p>altura: {data.height}</p>
-            <p>peso: {data.weight}</p>
-
-            {/* Exibindo os tipos do Pokémon */}
           </div>
         </div>
-        <div className="card-image">
-          <img src={actualImage} />
+        <div className="card-info">
+          <p>Nome: {data.name}</p>
+          <p>altura: {data.height}</p>
+          <p>peso: {data.weight}</p>
+
+          {/* Exibindo os tipos do Pokémon */}
         </div>
       </div>
     </div>
+    
   );
 }
