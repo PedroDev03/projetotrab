39101d0680772a9e98027eac138ba8489e7f9dc2
Pedro <pedro.soares@unimedanapolis.com.br>
2024-04-29 12:55:53 -0300

    alteracoes 29/04

--- src/App.jsx
+++ src/App.jsx
@@ -3,29 +3,49 @@ import "./index.css";
   import pokelogo from "./assets/pokelogo.png";
 export default function App() {
   const [data, setData] = useState(null);
+  const [isError, setIsError] = useState(null)
   const [pokemonName, setPokemonName] = useState("");
-  const [pokemonshiny , setPokemonshiiny] = useState(true);
+  const [pokemonshiny , setPokemonshiny] = useState(true);
 
-  useEffect(() => {
+
+
     const fetchData = async () => {
+      console.log('rodolfo');
       try {
         const response = await fetch(
           `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
         );
         const jsonData = await response.json();
         setData(jsonData);
+        console.log(response.status);
       } catch (error) {
         console.error("Erro ao buscar dados:", error);
+        setIsError(error)
+        setPokemonName('');
+        setPokemonshiny(true);
+    
+        // throw new Error ('404')
       }
     };
 
-    fetchData();
-  }, [pokemonName, data]);
+useEffect(() => {
+  
+ fetchData();
+}, [pokemonName])
+
+
+
 
   const handleInputChange = (event) => {
     const name = event.target.value;
     setPokemonName(name.toLowerCase());
+     fetchData();
+ 
   };
+
+
+
+
 
   if (!data) {
     return <>Carregando...</>;
@@ -69,7 +89,7 @@ export default function App() {
   
 
   const actualImageshiny = data?.sprites?.front_shiny || pokelogo;
-  const actualImage = data?.sprites?.front_default || pokelogo;
+  const actualImage = data?.sprites?.front_default  || pokelogo  ;
   const verify = data.types && data.types.length > 0 && data.types[0].type && data.types[0].type.name || data.types && data.types.length > 0 && data.types[1].type && data.types[1].type.name;
 
   const habilidade = data.abilities && data.abilities.length > 0 && data.abilities[0].ability && data.abilities[0].ability.name ;
@@ -89,26 +109,30 @@ export default function App() {
             <div className="tipo">Tipo: {verify ? data.types[0].type.name : '' || verify ? data.types[1].type.name : ''}</div>
           </div>
         
-         <div className="card-image">
-             {
-              pokemonName === '' ? (
-                <img src={pokelogo}/>
-                
-              ) : (
-                <>
-                        {<button className="shiny" onClick={() => setPokemonshiiny(!pokemonshiny)}>shiny</button>}
-                        
-                <img src={pokemonshiny ? actualImage : actualImageshiny} onClick={() => setPokemonshiiny(!pokemonshiny)}/>
+         <div className="card-image ">
 
-                </> 
-              )
+         <img
+           className={pokemonshiny ? "fade-in" : "hidden"}
+           src={actualImage}
+           alt="Normal"
+           onClick={() => setPokemonshiny(!pokemonshiny)}
+         />
+         <img
+           className={pokemonshiny ? "hidden" : "fade-in"}
+           src={actualImageshiny}
+           alt="Shiny"
+           onClick={() => setPokemonshiny(!pokemonshiny)}
+         />
+       </div>
+       
                    
-             }      
-             
+                  
+            
 
           </div>
-        </div>
+       
         <div className="card-info">
+        {!pokemonshiny && data.name ? <div className="aviso-shiny">shiny</div> : ''}
           <p>Nome: {data.name}</p>
           <p>altura: {data.height ? data.height/10  + ' m' : ''}  </p>
           <p>peso: {data.weight ? data.weight/10  + ' kg' : ''} </p>
