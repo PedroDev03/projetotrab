408391178562eb8e0208609a29436e9dc4f9a0ba
PedroDev <132094853+PedroDev03@users.noreply.github.com>
2024-07-29 12:56:44 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ export default function App()
 
     useEffect(() => {
       fetchData();
-    }, [pokemonName, fetchData]);
+    }, []);
   
     const handleInputChange = (event) => {
       const name = event.target.value;
